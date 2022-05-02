2609d36289757c4801268e1b24caf5e60447f44d
Dmitry757 <glv11@mail.ru>
2022-05-03 00:43:05 +0300

    Initial commit

---
+++ src/org/dng/Matrix.java
@@ -0,0 +1,204 @@
+package org.dng;
+
+import java.io.Serializable;
+
+
+/**
+ 1. Написать класс «Матрица». Данный класс реализует функционал математического объекта «матрица».
+ Обязательные поля класса:
+  элементы матрицы – двумерный массив
+  кол-во строк/кол-во столбцов – целочисленные значения
+
+ Обязательные методы класса:
+  конструкторы
+  деструктор
+  сеттеры/геттеры
+  метод получения/установки элемента матрицы aij, где i – номер строки, j – номер столбца
+  метод заполнения матрицы случайными значениями
+  сложение/вычитание двух матриц (поэлементно)
+
+ Бонусные методы (реализация не обязательная, по желанию, продвинутый уровень):
+  умножение матрицы на матрицу
+  вычисление определителя матрицы
+  метод записи матрицы в файл
+  метод чтения матрицы из файла
+
+ 2. При разработке класса «Матрица» выполнять обработку исключительных ситуаций:
+  при вводе/вывод, выделении памяти, доступах к элементам и т.д.
+  сделать новый класс собственного исключения MatrixException, наследника Exception, и использовать его для обработки
+  исключительных ситуаций при работе с матрицами (получение элемента, создание матрицы, операции с матрицами) –
+  пример исключительных ситуаций, требующих обработки – получение/установка элемента по несуществующему индексу,
+  сложение матриц разной размерности и т.д.
+
+ 3. Протестировать данный класс используя вызовы методов. Можно написать собственный CLI (command line interface)
+  простого калькулятора матриц.
+ */
+class MatrixException extends Exception{
+    public MatrixException(String message) {
+        super(message);
+    }
+}
+
+public class Matrix implements Serializable {
+
+    private int[][] matrix;
+    private int yDimension;
+    private int xDimension;
+
+    public Matrix(int yDimension, int xDimension) throws MatrixException {
+        // catch exception here - we can`t create instance of Matrix with wrong parameters
+        try {
+            this.yDimension = yDimension;
+            this.xDimension = xDimension;
+            this.matrix = createRandomMatrix(yDimension, xDimension, 100);
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+            throw new MatrixException(e.getMessage()); //and pass exception up
+        }
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+        this.yDimension = matrix.length;
+        this.xDimension = matrix[0].length;
+    }
+
+    /**
+     * set value val as element of matrix to position i,j
+     * @param i
+     * @param j
+     * @param val
+     * @throws MatrixException - checking the condition for the inclusion of indices in the dimension of the matrix
+     */
+    public void setMatrixElement(int i, int j, int val) throws MatrixException {
+        if((matrix.length<=i)||(matrix[0].length)<=j)
+            //throw new ArrayIndexOutOfBoundsException("dimension of the matrix is violated! Index out of range!");
+            throw new MatrixException("dimension of the matrix is violated! Index out of range!");
+
+        this.matrix[i][j] = val;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public int getYDimension() {
+        return yDimension;
+    }
+
+    public int getXDimension() {
+        return xDimension;
+    }
+
+    public void printMatrix() {
+        for (int[] y : matrix) {
+            System.out.print("[");
+            for (int x : y) {
+                System.out.printf("%3d", x);
+            }
+            System.out.print(" ]");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void fillMatrixRandom() throws MatrixException {
+        try {
+            //rndRange (value 49) is chosen so that when added, the result must be less than 100 - for good printing
+            this.matrix = createRandomMatrix(yDimension, xDimension, 49);
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+            throw new MatrixException(e.getMessage()); //and pass exception up
+        }
+
+    }
+    public static int[][] createRandomMatrix(int dimY, int dimX, int rndRange) throws MatrixException {
+        if ((dimY < 1) || (dimX < 1)) {
+//            throw new IllegalArgumentException("Wrong matrix dimension!");
+            throw new MatrixException("dimension of the matrix is violated! Index out of range!");
+
+        }
+        int[][] matrix = new int[dimY][dimX];
+        for (int i = 0; i < dimY; i++) {
+            for (int j = 0; j < dimX; j++) {
+                matrix[i][j] = (int) (Math.random() * rndRange);
+            }
+        }
+        return matrix;
+    }
+
+    public  Matrix matrixAddition(Matrix matrix2) throws MatrixException {
+        int[][] m1 = matrix;
+        int[][] m2 = matrix2.getMatrix();
+        //lets compare dimensions of matrix
+        if (m1.length != m2.length) {
+            throw new MatrixException("Error! Matrix Y-dimensions must be equal!");
+        }
+        if ((m1.length == 0) || (m2.length == 0)) {
+            throw new MatrixException("Error! Matrix dimensions must be >0 !");
+        }
+
+        for (int i = 0; i < m1.length; i++) {
+            if (m1[i].length != m2[i].length) {
+                throw new MatrixException("Error! Matrix X-dimensions must be equal!");
+            }
+        }
+
+
+        int y = Math.max(m1.length, m2.length);
+        int x = 0;
+        for (int[] d : m1) {
+            x = Math.max(x, d.length);
+        }
+        for (int[] d : m2) {
+            x = Math.max(x, d.length);
+        }
+
+        int[][] arr = new int[y][x];
+        for (int i = 0; i < (y); i++) {
+            for (int j = 0; j < x; j++) {
+                arr[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+
+        Matrix res = new Matrix(y,x);
+        res.setMatrix(arr);
+        return res;
+    }
+
+    //determinant calculation built using regression - so, need to make two methods
+    public int getDet() throws MatrixException {
+        return det(matrix);
+    }
+
+    public static int det(int[][] matrix) throws MatrixException {
+        int rez = 0;
+        int dimY = matrix.length, dimX = matrix[0].length;
+        if (dimY != dimX) {
+            throw new MatrixException("Error. The matrix is not square! determinant can`t be found!");
+        }
+
+        if ((dimY == 2) && (dimX == 2)) {
+            return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+        }
+
+
+        for (int y = 0; y < dimY; y++) {
+            int a = matrix[y][0]; //разложение по 1-му столбцу, оптимизацию писать лень ))
+            int[][] minor = new int[dimY - 1][dimX - 1]; //reduce of dimension
+
+            for (int i = 0; i < dimY; i++) {
+                if (i == y) continue;
+                int z = (i < y) ? i : (i - 1);
+                System.arraycopy(matrix[i], 1, minor[z], 0, minor[z].length);
+            }
+//            printMatrix(minor);
+//            System.out.println("a = " + (int)Math.pow((-1),(1+y+1))*a);
+            rez += (int) Math.pow((-1), (1 + y + 1)) * a * det(minor);
+        }
+        return rez;
+    }
+
+
+
+}
---
+++ src/org/dng/Test.java
@@ -0,0 +1,65 @@
+package org.dng;
+
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Matrix matrix1 = null, matrix2 = null;
+        try {
+            matrix1 = new Matrix(3, 3);
+            matrix1.fillMatrixRandom();
+            System.out.println("Matrix1 is: ");
+            matrix1.printMatrix();
+
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            matrix2 = new Matrix(3, 3);
+            matrix2.fillMatrixRandom();
+            System.out.println("Matrix2 is: ");
+            matrix2.printMatrix();
+
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println("Addition matrix1 and matrix2 is:");
+            matrix1.matrixAddition(matrix2).printMatrix();
+
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        //** lets try to serialize our instance of Matrix
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream("d:\\matrix.ser");
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        ) {
+            System.out.println("matrix for serialization is");
+            matrix1.printMatrix();
+            objectOutputStream.writeObject(matrix1);
+            //objectOutputStream.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //** now lets try to deserialize matrix.ser to instance of Matrix
+        try (
+                FileInputStream fileInputStream  = new FileInputStream("d:\\matrix.ser");
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        ) {
+            Matrix matrixDeSer = (Matrix) objectInputStream.readObject();
+            //objectOutputStream.close();
+            System.out.println("DeSerialized matrix is");
+            matrixDeSer.printMatrix();
+        } catch (ClassNotFoundException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
