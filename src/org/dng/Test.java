d3043c1fe404b4854066cdd9bab48e577919112c
Dmitry757 <glv11@mail.ru>
2022-05-10 14:28:25 +0300

    add some exceptions to Test

--- src/org/dng/Test.java
+++ src/org/dng/Test.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Test {
     public static void main(String[] args) {
-        Matrix matrix1 = null, matrix2 = null;
+        Matrix matrix1 = null, matrix2 = null, matrix3 = null;
         try {
             matrix1 = new Matrix(3, 3);
             matrix1.fillMatrixRandom();
@@ -16,6 +16,12 @@ public class Test {
             matrix2.fillMatrixRandom();
             System.out.println("Matrix2 is: ");
             matrix2.printMatrix();
+            System.out.println();
+
+            matrix3 = new Matrix(3, 4);
+            matrix3.fillMatrixRandom();
+            System.out.println("Matrix3 is: ");
+            matrix3.printMatrix();
             System.out.println();
 
             System.out.println("Addition matrix1 and matrix2 is:");
@@ -34,11 +40,31 @@ public class Test {
             System.out.println(e.getMessage());
         }
 
+        try{
+            System.out.println("Try Add matrix3 to matrix1 (matrix have different dimensions");
+            matrix1.matrixAdd(matrix3);
+            matrix1.printMatrix();
+            System.out.println();
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            System.out.println();
+            System.out.println("let`s calc determinant of non square matrix");
+            System.out.println("the determinant is " + matrix3.getDet());
+            System.out.println();
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+        }
+
 
         //** lets try to serialize our instance of Matrix
         try {
             if (matrix1 != null) {
+                System.out.println();
                 matrix1.serialize();
+                System.out.println("Serialization......");
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -46,10 +72,10 @@ public class Test {
 
 
         //** now lets try to deserialize matrix.ser to instance of Matrix
-        Matrix matrix3 = null;
+        Matrix matrix4 = null;
         try {
-            matrix3 = new Matrix(3,3);
-            matrix3.deSerialize();
+            matrix4 = new Matrix(3,3);
+            matrix4.deSerialize();
         } catch (MatrixException | ClassNotFoundException | IOException e) {
             System.out.println(e.getMessage());
         }
